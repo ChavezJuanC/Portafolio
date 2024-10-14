2145cd86c1dd965eecdd84d0925df0e960bb3e39
Juan <dosomethingwild012712@gmail.com>
2024-10-14 13:26:22 -0600

    //Code reformatted for skill cards//

--- src/assets/components/MainRoutes/Skills.tsx
+++ src/assets/components/MainRoutes/Skills.tsx
@@ -2,6 +2,59 @@ import React from "react";
 import SkillList from "../OtherComponents/SkillList";
 
 const Skills = (): JSX.Element => {
+    interface skillsInt {
+        skillType: string;
+        skills: string[];
+    }
+
+    const skillsArr: skillsInt[] = [
+        {
+            skillType: "Languages",
+            skills: [
+                "JavaScript (ES6+)",
+                "TypeScript",
+                "Python 3",
+                "C#",
+                "GDScript",
+                "HTML / CSS",
+            ],
+        },
+        {
+            skillType: "Frontend Development",
+            skills: [
+                "React.js",
+                "Next.js",
+                "Tailwind CSS",
+                "React Native (Mobile Development)",
+            ],
+        },
+        {
+            skillType: "Backend Development",
+            skills: ["Express.js", "Flask", "Django", "FastAPI"],
+        },
+        {
+            skillType: "Databases",
+            skills: ["MongoDB", "Firebase", "SQL databases"],
+        },
+        {
+            skillType: "Tools & Platforms",
+            skills: ["Git & GitHub", "NPM / Yarn", "Linux / Unix systems"],
+        },
+        {
+            skillType: "Game Development",
+            skills: ["Unity (C#)", "Godot (GDScript)", "2D Game Design"],
+        },
+    ];
+
+    const skillCards = skillsArr.map((element) => (
+        <SkillList title={element.skillType}>
+            <ul className="list-disc list-inside pl-3 grid text-md text-customBlack">
+                {element.skills.map((skill) => (
+                    <li className="my-2">{skill}</li>
+                ))}
+            </ul>
+        </SkillList>
+    ));
 
     return (
         <div>
@@ -9,55 +62,7 @@ const Skills = (): JSX.Element => {
                 MY SKILLS
             </h1>
             <div className="lg:w-3/4 mx-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 justify-items-center">
-                <SkillList title="Languages">
-                    <ul className="list-disc list-inside pl-3 grid text-md text-customBlack">
-                        <li className="my-2">JavaScript (ES6+)</li>
-                        <li className="my-2">TypeScript</li>
-                        <li className="my-2">Python3</li>
-                        <li className="my-2">C#</li>
-                        <li className="my-2">GDScript</li>
-                        <li>HTML / CSS</li>
-                    </ul>
-                </SkillList>
-                <SkillList title="Frontend Development">
-                    <ul className="list-disc list-inside pl-5 grid text-md text-customBlack">
-                        <li className="my-2">React.js</li>
-                        <li className="my-2">Next.js</li>
-                        <li className="my-2">Tailwind CSS</li>
-                        <li className="my-2">
-                            React Native (Mobile Development)
-                        </li>
-                    </ul>
-                </SkillList>
-                <SkillList title="Backend Development">
-                    <ul className="list-disc list-inside pl-5 grid text-md text-customBlack">
-                        <li className="my-2">Express.js</li>
-                        <li className="my-2">Flask</li>
-                        <li className="my-2">Django</li>
-                        <li className="my-2">FastAPI</li>
-                    </ul>
-                </SkillList>
-                <SkillList title="Databases">
-                    <ul className="list-disc list-inside pl-5 grid text-md text-customBlack">
-                        <li className="my-2">MongoDB</li>
-                        <li className="my-2">Firebase</li>
-                        <li className="my-2">SQL</li>
-                    </ul>
-                </SkillList>
-                <SkillList title="Tools & Platforms">
-                    <ul className="list-disc list-inside pl-5 grid text-md text-customBlack">
-                        <li className="my-2">Git & GitHub</li>
-                        <li className="my-2">NPM / Yarn</li>
-                        <li className="my-2">Linux / Unix systems</li>
-                    </ul>
-                </SkillList>
-                <SkillList title="Game Development:">
-                    <ul className="list-disc list-inside pl-5 grid text-md text-customBlack">
-                        <li className="my-2">Unity (C#)</li>
-                        <li className="my-2">Godot (GDScript)</li>
-                        <li className="my-2">2D Game Design</li>
-                    </ul>
-                </SkillList>
+                {skillCards}
             </div>
         </div>
     );
@@ -65,6 +70,3 @@ const Skills = (): JSX.Element => {
 
 export default Skills;
 
-
-///To Do
-//Reformat code into an interface, an array of objs and than map it.
